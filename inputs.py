ba673c7f56ff3bb395b20bd797c093c6413173d0
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2026-03-17 07:40:44 +0600

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,9 @@
+a = input("Enter A Number")
+b = input("Enter Another Number ")
+num1 = int(a)
+num2 = int(b)
+
+print(f"Sum = {num1 + num2}")
+print(f"Substraction = {num1 - num2}")
+print(f"Division = {num1 / num2}")
+print(f"Multiplication = {num1 * num2}")
---
+++ web.py
@@ -0,0 +1,9 @@
+import webbrowser
+import pyttsx3
+#webbrowser.open('https://www.google.com')
+
+engine = pyttsx3.init();
+engine.say("Welcome Sadman to Python World")
+engine.runAndWait()
+
+
